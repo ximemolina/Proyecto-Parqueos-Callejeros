719381a561e90a3e23b0dddcaaecd963448b9242
juanpaa08 <juanpacamal08@gmail.com>
2024-10-10 20:15:48 -0600

    Actualizacion de clase Carro
    Hubo un error en el commit anterior, este actualiza la clase Carro.

--- ProyectoParqueos/src/main/java/com/mycompany/proyectoparqueos/Carro.java
+++ ProyectoParqueos/src/main/java/com/mycompany/proyectoparqueos/Carro.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.proyectoparqueos;
 
-/**
- *
- * @author Admin
- */
 public class Carro {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private Cliente propietario;
     
+    public Carro(String pPlaca, String pMarca, String pModelo, Cliente pPropietario){
+        setPlaca(pPlaca);
+        setMarca(pMarca);
+        setModelo(pModelo);
+        propietario = pPropietario;
+    }
+    
+    public void setPlaca(String pPlaca){
+        if (String.valueOf(pPlaca).length() < 1 || String.valueOf(pPlaca).length() > 6){
+            throw new IllegalArgumentException("La placa debe tener entre 1 y 6 caracteres.");
+        }
+        this.placa = pPlaca;
+    }
+    
+    public void setMarca(String pMarca){
+        if (String.valueOf(pMarca).length() < 0 || String.valueOf(pMarca).length() > 15){
+            throw new IllegalArgumentException("La marca debe tener entre 0 y 15 caracteres.");
+        }
+        this.marca = pMarca;
+    }
+    
+    public void setModelo(String pModelo){
+        if (String.valueOf(pModelo).length() < 0 || String.valueOf(pModelo).length() > 15){
+            throw new IllegalArgumentException("El modelo debe tener entre 0 y 15 caracteres.");
+        }
+        this.modelo = pModelo;
+    }
+    
+    public String getPlaca(){
+        return placa;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public String getModelo(){
+        return modelo;
+    }
+      
 }
